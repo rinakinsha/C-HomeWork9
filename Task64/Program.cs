9f834412c3ba6d1f7385991783d5a95f36716310
iryna <rinakinsha@yndex.by>
2022-07-21 22:17:59 +0300

    Solution of task 64

---
+++ Task64/Program.cs
@@ -0,0 +1,12 @@
+﻿int NumberOfDigits(int N)
+{
+    if (N / 10 != 0)
+        return 1 + NumberOfDigits(N / 10);
+
+    else return 1;
+}
+
+Console.Write("Пожалуйста, введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Количетсво цифр в указанном числе: " + NumberOfDigits(m));
