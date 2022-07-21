5e6efc590b00c090819443b0be22f00ed13ee6c2
iryna <rinakinsha@yndex.by>
2022-07-21 23:11:57 +0300

    Solution of task 66

---
+++ Task66/Program.cs
@@ -0,0 +1,18 @@
+﻿int SumOfNaturalElem(int M, int N)
+{
+    if (M < N)
+    {
+        while (M != N)
+            return M + SumOfNaturalElem(M + 1, N);
+    }
+
+    if (M > N)
+    {
+        while (M != N)
+            return M + SumOfNaturalElem(M - 1, N);
+    }
+
+    return M;
+}
+
+Console.Write(SumOfNaturalElem(8, 4));
